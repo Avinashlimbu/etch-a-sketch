f1fa39b0d857ef4c3c95284210b9aba541a9c572
Avinash <50650789+Avinashlimbu@users.noreply.github.com.>
2025-05-24 13:22:17 +0900

    added mouse neter and mouse leave function
    pretty stratght forwards

--- script.js
+++ script.js
@@ -2,21 +2,24 @@
 
 const container = document.getElementById('container');
 
-
 function createDivs (numberOfDivs) {
     for (let i = 0; i < numberOfDivs; i++) {
     const innerDiv = document.createElement('div');
     innerDiv.classList.add('innerDiv');
     container.appendChild(innerDiv);
+
+    //mouse over event for the innner Divs
+            innerDiv.addEventListener("mouseenter", function(){
+            this.style.backgroundColor = "lightblue";
+        });
+            innerDiv.addEventListener("mouseleave", function(){
+            this.style.backgroundColor = "";
+        });
     }
 }
-
-
 createDivs(512/2);
 
-/*
-Basic pseudo code for my understanding! 
-making a sketch pad project
 
 
-*/
+
+
